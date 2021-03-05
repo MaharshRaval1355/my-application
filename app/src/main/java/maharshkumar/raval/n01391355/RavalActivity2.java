88481074c78ba3cefa0f87b27e7b6c8450ccbe69
MaharshRaval1355 <n01391355@gmail.com>
2021-03-04 21:09:33 -0500

    I did what I could do.

---
+++ app/src/main/java/maharshkumar/raval/n01391355/RavalActivity2.java
@@ -0,0 +1,14 @@
+package maharshkumar.raval.n01391355;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RavalActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raval2);
+    }
+}
